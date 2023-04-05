86891693b85cc4094f872f28af4105c60e50d533
Kajal <121721088+kjlconcern@users.noreply.github.com>
2023-04-05 23:47:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// var, let and const
+
+//let and const block scoped
+
+
+console.log(y)
+
+let y =5 
+
+// var x =5;
+
+
+
+let person = {
+    name: "Devarshi",
+    age:25,
+
+}
+
+console.log(this)
+
+if(true){
+    let y = 5
+    y = 7
+
+    console.log(y) //5
+}
+
+
+console.log(y) //5
+
